aef37650230c21e27500c00fb3bddc82150730ec
Jan <jan.pimia@gmail.com>
2021-09-14 13:46:29 +0300

    2nd Task

---
+++ homework/2021-37/e02/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String sana = new String("      jorma       ");
+
+        System.out.println(sana.charAt(7));
+        System.out.println(sana.length()-1);
+        System.out.print(sana.trim());
+    }
+}
