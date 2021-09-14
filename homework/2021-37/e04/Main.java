e96d4c7fd4e538eadf96d1ebb7c8b118aaea75ce
Jan <jan.pimia@gmail.com>
2021-09-14 14:58:08 +0300

    4th Task

---
+++ homework/2021-37/e04/Main.java
@@ -0,0 +1,14 @@
+import java.io.Console;
+
+public class Main {
+    public static void main(String[] args) {
+        Console c = System.console();
+
+        System.out.println("Anna jokin nimi:");
+        String nimi = c.readLine();
+
+        for(int i = nimi.length()-1; i >= 0; i--) {
+            System.out.println(nimi.charAt(i));
+        }
+    }
+} 
