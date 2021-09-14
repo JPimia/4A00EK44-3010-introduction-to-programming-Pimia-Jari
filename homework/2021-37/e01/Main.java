344d9c0e5db45b281a5555b67b5c6750c2223a9f
Jan <jan.pimia@gmail.com>
2021-09-14 13:43:02 +0300

    Eka tehtävä

--- homework/2021-37/e01/Main.java
+++ homework/2021-37/e01/Main.java
@@ -1,6 +1,16 @@
+import java.io.Console;
 
-class Main {
-    public static void main(String [] args) {
-        System.out.println("Hello world");
+public class Main {
+    public static void main(String[] args) {
+        Console c = System.console();
+
+        System.out.println("Anna nimesi?");
+        String nimi = c.readLine();
+
+        if(nimi.equals("jussi")){
+            System.out.println("On sulla tyhma nimi");
+        } else {
+            System.out.println("onpas sulla hieno nimi");
+        }
     }
 }
