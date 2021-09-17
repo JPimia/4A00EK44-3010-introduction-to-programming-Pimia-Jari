35ed6efca5f66d3589b1eee3be07b65c0b29fa8d
JPimia <jari.pimia@tuni.fi>
2021-09-18 01:13:49 +0300

    8th Task

---
+++ homework/2021-37/e08/Main.java
@@ -0,0 +1,21 @@
+import java.io.Console;
+
+public class Main {
+    public static void main(String[] args) {
+        Console c = System.console();
+        String kaannettyjono = "";
+
+        System.out.println("anna merkkijono");
+        String merkkijono = c.readLine();
+
+        for(int i = merkkijono.length()-1; i >= 0; i--) {
+            kaannettyjono = kaannettyjono + merkkijono.charAt(i);
+        }
+        
+        if(merkkijono.equals(kaannettyjono)){
+            System.out.println("Merkkijono on palindromi");
+        } else {
+            System.out.println("Merkkijono ei ole palindromi");
+        }
+    }
+}
