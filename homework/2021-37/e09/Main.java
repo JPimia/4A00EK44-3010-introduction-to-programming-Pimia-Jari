74f53999c38ffa1c8801a46e8fbf2503183c78c7
JPimia <jari.pimia@tuni.fi>
2021-09-18 03:37:43 +0300

    9th Task

---
+++ homework/2021-37/e09/Main.java
@@ -0,0 +1,34 @@
+import java.io.Console;
+
+public class Main {
+    public static void main(String[] args) {
+        Console c = System.console();
+        String kaannettyjono = "";
+        String muokattujono = "";
+
+        System.out.println("anna merkkijono");
+        String merkkijono = c.readLine();
+
+        for(int i = merkkijono.length()-1; i >= 0; i--) { //käy merkkijonon läpi väärinpäin
+            char merkki = merkkijono.charAt(i); //hakee yksittäisen merkin
+            
+            if(merkki != ',' && merkki != '.' && merkki != ' '){ //poistaa nämä merkit
+                kaannettyjono = kaannettyjono + merkki;
+            }
+        }
+
+        for(int i = 0; i < merkkijono.length(); i++) { //käy merkkijonon läpi oikeinpäin
+            char merkki = merkkijono.charAt(i); //hakee yksittäisen merkin
+            
+            if(merkki != ',' && merkki != '.' && merkki != ' '){ //poistaa nämä merkit
+                muokattujono = muokattujono + merkki;
+            }
+        }
+        
+        if(muokattujono.equalsIgnoreCase(kaannettyjono)){
+            System.out.println("Merkkijono on palindromi");
+        } else {
+            System.out.println("Merkkijono ei ole palindromi");
+        }
+    }
+}
