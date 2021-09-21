63405b0a29387a8bc91fd0d4a03d72b6b3ab4bdd
JPimia <jari.pimia@tuni.fi>
2021-09-21 03:55:37 +0300

    2nd Task

--- homework/2021-38/e02/Main.java
+++ homework/2021-38/e02/Main.java
@@ -1,23 +1,23 @@
 
 class Main {
     public static void main(String [] args) {
-       // luodaan taulukko jonka koko on 3. Sisältää 3 nollaa
+        // luodaan taulukko jonka koko on 3. Sisältää 3 nollaa
         int [] taulukko = new int[3];  
 
-       // korvataan nollat arvoilla 101, 200 ja 999     
+        // korvataan nollat arvoilla 101, 200 ja 999     
         taulukko[0] = 12;
         taulukko[1] = 99;
         taulukko[2] = 32;
         
         /*tulostetaan arvot
         System.out.println(taulukko[0]);
-        //System.out.println(taulukko[1]);       
+        //System.out.println(taulukko[1]);
         System.out.println(taulukko[2]);
 
-       // yhdellä rivillä taulukon luonti, koon antaminen ja arvojen antaminen
+        // yhdellä rivillä taulukon luonti, koon antaminen ja arvojen antaminen
         //int [] toinen = {3,4,5,6};
         
-       // taulukon koko*/
+        // taulukon koko*/
                 //System.out.println(toinen.length);
 
                 System.out.println("Taulukon pituus on: " + taulukko.length);
