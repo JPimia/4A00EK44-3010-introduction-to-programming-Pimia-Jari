8a462986666a390d6acb57da43bf582c7f502eb2
JPimia <jari.pimia@tuni.fi>
2021-09-21 16:59:42 +0300

    9th Task

--- homework/2021-38/e09/Main.java
+++ homework/2021-38/e09/Main.java
@@ -3,15 +3,15 @@ import java.io.Console;
 class Main {
     public static void main(String [] moooorjesta) {
         Console c = System.console();
-       // luodaan taulukko jonka koko on 3. Sisältää 3 nollaa        
         String longestName = "";
         String name = "";
 
+        // annetaan nimet ja taulukon paikkojen määrä
         System.out.print("Number of names? ");
         int numberOfNames = Integer.parseInt(c.readLine());
         String [] taulukko = new String[numberOfNames +1];
 
-        // annetaan nimet ja katsotaan taulukosta pisin nimi
+        // katsotaan taulukosta pisin nimi
         for(int i = 1; i <= numberOfNames; i++){
             System.out.print(i + ". " + "name: ");
                 name = c.readLine();
