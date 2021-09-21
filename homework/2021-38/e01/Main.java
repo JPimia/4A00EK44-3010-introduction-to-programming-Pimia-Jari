4e89f14382857e17696836cc783792d8801ff3fc
JPimia <jari.pimia@tuni.fi>
2021-09-21 03:53:54 +0300

    1st Task

--- homework/2021-38/e01/Main.java
+++ homework/2021-38/e01/Main.java
@@ -4,14 +4,14 @@ class Main {
        // luodaan taulukko jonka koko on 3. Sisältää 3 nollaa
         int [] taulukko = new int[3];  
 
-       // korvataan nollat arvoilla 101, 200 ja 999     
+       // korvataan nollat arvoilla 101, 200 ja 999
         taulukko[0] = 12;
         taulukko[1] = 99;
         taulukko[2] = 32;
         
        // tulostetaan arvot
         System.out.println(taulukko[0]);
-        //System.out.println(taulukko[1]);       
+        //System.out.println(taulukko[1]);
         System.out.println(taulukko[2]);
 
        // yhdellä rivillä taulukon luonti, koon antaminen ja arvojen antaminen
