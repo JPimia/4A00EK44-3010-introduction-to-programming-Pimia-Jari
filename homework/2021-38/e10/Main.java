6c2471d7b6555c42e3470dff3790152108163258
JPimia <jari.pimia@tuni.fi>
2021-09-23 22:54:24 +0300

    10th Task

--- homework/2021-38/e10/Main.java
+++ homework/2021-38/e10/Main.java
@@ -1,26 +1,37 @@
 import java.io.Console;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Console c = System.console();
         boolean loop = true;
+        int indeksi = 0;
 
 
         System.out.println("Anna nimia");
         String [] nimiTaulukko = new String[10];
 
         do {
-            for(int i = 0; i < nimiTaulukko.length; i++){
-                
-                nimiTaulukko[i] = c.readLine();
+            String name = c.readLine(); // kysytään eka nimiä
+            if(name.equalsIgnoreCase("lopeta")){
+                loop = false;
+            } else {
+                nimiTaulukko[indeksi] = name; // tallennetaan nimiä taulukkoon
+                indeksi++;
 
-                if(nimiTaulukko[i].equals("lopeta")){
-                    loop = false;
-                    break;
+                if(indeksi >= nimiTaulukko.length){ // jos taulukko on täynnä
+                    String [] newArray = new String [2*nimiTaulukko.length]; // tee uusi joka on 2x vanha taulukko
+                    for(int i = 0; i < nimiTaulukko.length; i++){ 
+                        newArray[i] = nimiTaulukko[i]; // siirretään sisältö vanhasta uuteen
+                    }
+                    nimiTaulukko = newArray; // korvaa vanha uudella
                 }
             }
         } while (loop);
-        
+
+        System.out.println("annetut nimet:");
+
+        for(int i = 0; i < indeksi; i++){
+            System.out.println(nimiTaulukko[i]);
+        }
     }
 }
