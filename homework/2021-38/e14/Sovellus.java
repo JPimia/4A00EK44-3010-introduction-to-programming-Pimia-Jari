57812a0eec38c00baec4825aefcecefff909f1c2
JPimia <jari.pimia@tuni.fi>
2021-09-26 20:53:14 +0300

    14th Task

--- homework/2021-38/e14/Sovellus.java
+++ homework/2021-38/e14/Sovellus.java
@@ -2,21 +2,21 @@ public class Sovellus {
     public static void main(String[] args) {
         int[] numbers = new int [10];
         int argument = Integer.parseInt(args[0]);
-        boolean stop = true;
+        boolean stop = false;
         
 
         for(int i = 0; i < numbers.length; i++){
             numbers[i] = (int) (Math.random() * 100);
             
 
-            if(numbers[i] == argument){
-                System.out.println("Loytyi!");
-                stop = false;
-            } else if(i == numbers.length-1) {
-                System.out.println("Ei loytynyt!");                
+            if(numbers[i] == argument){ // katsotaan löytykö luku
+                System.out.println("Loytyi!"); 
+                stop = true; // jos löyty ohjelma sammuu
+            } else if(i == numbers.length-1) { // jos ei löydy ja taulukko on täys, tulostetaan ei löytynyt
+                System.out.println("Ei loytynyt!");
             }
-            
-            if(stop == false){
+
+            if(stop == true){ // luku löyty ja ohjelma sammmuu
                 break;
             }
         }
