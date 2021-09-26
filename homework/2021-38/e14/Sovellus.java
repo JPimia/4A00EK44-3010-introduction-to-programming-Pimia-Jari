09c9cc286ccc8b05d13bce52953b6eebc485ea19
JPimia <jari.pimia@tuni.fi>
2021-09-26 20:49:08 +0300

    14th Task

--- homework/2021-38/e14/Sovellus.java
+++ homework/2021-38/e14/Sovellus.java
@@ -2,6 +2,7 @@ public class Sovellus {
     public static void main(String[] args) {
         int[] numbers = new int [10];
         int argument = Integer.parseInt(args[0]);
+        boolean stop = true;
         
 
         for(int i = 0; i < numbers.length; i++){
@@ -10,9 +11,13 @@ public class Sovellus {
 
             if(numbers[i] == argument){
                 System.out.println("Loytyi!");
-            } else {
-                System.out.println("Ei loytynyt!");
-
+                stop = false;
+            } else if(i == numbers.length-1) {
+                System.out.println("Ei loytynyt!");                
+            }
+            
+            if(stop == false){
+                break;
             }
         }
     }
