39da02a83591cec2f80687d994711eeef6e3d10b
JPimia <jari.pimia@tuni.fi>
2021-09-27 21:19:00 +0300

    4th Task

---
+++ homework/2021-39/e04/E4.java
@@ -0,0 +1,9 @@
+
+class E4 {
+    public static void main(String [] args) {
+        System.out.println(palautaPaivamaara());
+    }
+    public static String palautaPaivamaara() {
+        return "14.9.2021";
+    }
+}
