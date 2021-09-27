893abc60bc97fc33477e841497d2433e1c859e83
JPimia <jari.pimia@tuni.fi>
2021-09-27 23:09:19 +0300

    8th Task

---
+++ homework/2021-39/e08/E8.java
@@ -0,0 +1,26 @@
+import  java.io.Console;
+
+public class E8 {
+    public static void main(String[] args) {
+        Console c = System.console();
+
+        System.out.print("Kuinka korkea tehdaan: ");
+        int korkeus = Integer.parseInt(c.readLine());
+
+        square(korkeus);
+    }
+
+    public static String charRepeat (String merkki, int amount){
+        String mj = "";
+        for(int i = 0; i < amount; i++){
+            mj += merkki;
+        }
+        return mj;
+    }
+
+    public static void square (int korkeus){
+        for(int i = 0; i < korkeus;  i++){
+            System.out.println(charRepeat("X", korkeus));
+        }
+    }
+}
