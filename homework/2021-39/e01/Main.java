b59ac4d6ebbcfdceca574356ed80429ac8df878d
Jan <jan.pimia@gmail.com>
2021-09-27 14:04:41 +0300

    1st Task

---
+++ homework/2021-39/e01/Main.java
@@ -0,0 +1,9 @@
+
+class Main {
+    public static void main(String [] args) {
+        tulostaNimi();
+    }
+    public static void tulostaNimi() {
+        System.out.println("Jari Pimia");
+    }
+}
