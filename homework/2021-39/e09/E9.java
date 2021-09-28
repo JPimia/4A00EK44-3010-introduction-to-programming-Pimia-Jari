10e78255e24c8f7cdc861c822aea2cf184f9ed9c
JPimia <jari.pimia@tuni.fi>
2021-09-28 22:17:47 +0300

    9th Task

---
+++ homework/2021-39/e09/E9.java
@@ -0,0 +1,28 @@
+
+class E9 {
+    static int a = 5;
+    static int b = 5;
+    static int summa = a + b;
+    public static void main(String [] args) {
+        asetaA();
+        asetaB();
+        laskeYhteen();
+        tulosta();
+    }
+
+    public static void asetaA() {
+        int a = 5;
+    }
+
+    public static void asetaB() {
+        int b = 5;
+    }
+
+    public static void laskeYhteen() {
+    
+    }
+
+    public static void tulosta() {
+        System.out.println(summa);
+    }
+}
