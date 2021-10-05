c5fc0ab16356edf4493316a239f4fb56c7f1814e
JPimia <jari.pimia@tuni.fi>
2021-10-05 18:05:02 +0300

    2nd Exercise

---
+++ homework/2021-40/e02/MyApp.java
@@ -0,0 +1,11 @@
+
+class MyApp {
+    public static void main(String [] args) {
+        System.out.println( tulostaPaivamaara() );
+    }
+
+    public static String tulostaPaivamaara() {
+        String pvm = "2018-09-16";
+        return pvm;
+    }
+}
