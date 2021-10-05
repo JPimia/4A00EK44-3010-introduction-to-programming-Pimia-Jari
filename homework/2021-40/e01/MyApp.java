5a580209647fc0e83fc130ccbf30cdb0ae561282
JPimia <jari.pimia@tuni.fi>
2021-10-05 18:04:49 +0300

    1st Exercise

---
+++ homework/2021-40/e01/MyApp.java
@@ -0,0 +1,11 @@
+
+class MyApp {
+    public static void main(String [] args) {
+        tulostaPaivamaara();
+    }
+
+    public static void tulostaPaivamaara() {
+        String pvm = "2018-09-16";
+        System.out.println(pvm);
+    }
+}
