e94421018189c906ff84cb01b8438228d85438ed
JPimia <jari.pimia@tuni.fi>
2021-10-16 21:33:36 +0300

    2nd Exercise

---
+++ homework/2021-41/e02/E2.java
@@ -0,0 +1,8 @@
+public class E2 {
+    public static void main(String[] args) {
+        int[][] array = {{1,3,9}, {2,7,3}};
+        for(int i = 0; i < array[0].length; i++) {
+            System.out.print(array[0][i]);
+        }
+    }
+}
