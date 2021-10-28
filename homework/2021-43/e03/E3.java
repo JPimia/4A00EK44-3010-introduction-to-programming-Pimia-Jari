6aaa219c5b9b65bf89a8a3ebdd49e966a9c725fa
JPimia <jari.pimia@tuni.fi>
2021-10-28 23:06:11 +0300

    3rd Robin Exercise

---
+++ homework/2021-43/e03/E3.java
@@ -0,0 +1,15 @@
+public class E3 {
+    public static void main(String[] args) {
+        int number = 2;
+        int number2 = 12;
+
+        while(number <= 12) {
+            System.out.println(number);
+            number++;
+        }
+        while(number2 > 1) {
+            System.out.println(number2);
+            number2--;
+        }
+    }
+}
