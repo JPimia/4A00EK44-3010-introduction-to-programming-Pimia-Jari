9b0f8112ca25610160feb0e0bf3b5266feae4d25
JPimia <jari.pimia@tuni.fi>
2021-11-01 20:22:23 +0200

    4th Robin Exercise

---
+++ homework/2021-43/e04/E4.java
@@ -0,0 +1,10 @@
+public class E4 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 15; i++) {
+            System.out.println(i);
+        }
+        for(int i = 15; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
