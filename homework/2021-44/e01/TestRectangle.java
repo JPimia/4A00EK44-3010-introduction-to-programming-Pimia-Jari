19c3f85b8722ce3013476896f9ee3a0a0ec68bcf
JPimia <jari.pimia@tuni.fi>
2021-11-03 00:57:16 +0200

    1st OOP Exercise

---
+++ homework/2021-44/e01/TestRectangle.java
@@ -0,0 +1,7 @@
+public class TestRectangle {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle();
+        System.out.println(rect.width + ", " + rect.height);
+        //tulostaa 0, 0 koska width ja height ovat oletusarvoisestin 0
+    }
+}
