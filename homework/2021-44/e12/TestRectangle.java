a123a80e1596b0f8895b480d77d3418a28726da8
JPimia <jari.pimia@tuni.fi>
2021-11-07 20:46:08 +0200

    12th OOP Exercise

---
+++ homework/2021-44/e12/TestRectangle.java
@@ -0,0 +1,11 @@
+
+public class TestRectangle {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle (50,50);
+        Rectangle r2 = new Rectangle (50,50);
+
+        if(r1.equals(r2)) {
+            //tänne mennään!
+        }
+    }
+}
