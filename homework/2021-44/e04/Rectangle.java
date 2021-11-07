ae355f75e24d8dad1866b48baa0914d8b9e67861
JPimia <jari.pimia@tuni.fi>
2021-11-07 16:11:09 +0200

    4th OOP Exercise

---
+++ homework/2021-44/e04/Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle {
+    public int width;
+    public int height;
+
+    public void whoAmI () {
+        System.out.println(this);
+        //viittaa nykyiseen objektiin metodissa tai constructorissa.
+    }
+}
---
+++ homework/2021-44/e04/TestRectangle.java
@@ -0,0 +1,7 @@
+
+public class TestRectangle {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle();
+        rect.whoAmI();
+    }
+}
