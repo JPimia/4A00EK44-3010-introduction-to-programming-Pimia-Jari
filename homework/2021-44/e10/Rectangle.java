122ba4372b82efbb2b66c4f01b21868bf25014de
JPimia <jari.pimia@tuni.fi>
2021-11-07 20:29:55 +0200

    10th OOP Exercise

---
+++ homework/2021-44/e10/Rectangle.java
@@ -0,0 +1,31 @@
+public class Rectangle {
+    private int height;
+    private int width;
+
+    public Rectangle(int width, int height) {
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public void setWidth (int width) {
+        if(width < 0) {
+            throw new IllegalArgumentException("Anna positiivinen luku");
+        } else {
+            this.width = width;
+        }
+        
+    }
+    public void setHeight (int height) {
+        if(height < 0) {
+            throw new IllegalArgumentException("Anna positiivinen luku");
+        } else {
+            this.height = height;
+        }
+    }
+    public int getWidth () {
+        return width;
+    }
+    public int getHeight () {
+        return height;
+    }
+}
---
+++ homework/2021-44/e10/TestRectangle.java
@@ -0,0 +1,8 @@
+
+public class TestRectangle {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle (50,50);
+        System.out.println(r.getWidth());
+        System.out.println(r.getHeight());
+    }
+}
