13f5138d9178f4fc9b48aea34371bd51436cedda
JPimia <jari.pimia@tuni.fi>
2021-11-13 19:57:52 +0200

    4th OOP Exercise

---
+++ homework/2021-45/e04/Testi.java
@@ -0,0 +1,55 @@
+
+
+class Ihminen {
+    private String name;
+
+    public Ihminen(String n) {
+        setName(n);
+    }
+
+    public void setName (String n) {
+        if(n.length() <= 0) {
+            throw new IllegalArgumentException("liian lyhyt nimi");
+        } else {
+            this.name = n;
+        }
+    }
+    public String getName() {
+        return name;
+    }
+    public void nuku () {
+        System.out.println(name + " ei nuku ikina");
+    }
+    public void syo () {
+        System.out.println(name + " syo pizzaa");
+    }
+    public void juo () {
+        System.out.println(name + " juo kaljaa");
+    }
+    public void lisaanny () {
+        System.out.println(name + " lisaantyy");
+    }
+    
+}
+
+class Ohjelmoija extends Ihminen {
+    //Construktori ei periydy.
+    public void osallistu() {
+        System.out.println(getName() + " bilettaa");
+    }
+    public void ohjelmoi() {
+        System.out.println(getName() + " ohjelmoi");
+    }
+}
+
+public class Testi {
+    public static void main(String[] args) {
+        Ohjelmoija jeppe = new Ohjelmoija("Jeesus");
+        jeppe.nuku();
+        jeppe.syo();
+        jeppe.juo();
+        jeppe.lisaanny();
+        jeppe.osallistu();
+        jeppe.ohjelmoi();
+    }
+}
