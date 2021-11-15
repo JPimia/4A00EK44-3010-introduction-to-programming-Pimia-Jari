48dc62b33e49bfcf56fd30b292b1aedc2a264747
Jan <jan.pimia@gmail.com>
2021-11-15 14:42:17 +0200

    2nd Task

---
+++ homework/2021-46/e02/Main.java
@@ -0,0 +1,14 @@
+
+
+class Main {
+    public static void main(String [] args) {
+        String lintu = "pulu lentaa";
+
+        class Bird {
+            public Bird() {
+                System.out.println(lintu);
+            }
+        }
+        Bird pulu = new Bird();
+    }
+}
