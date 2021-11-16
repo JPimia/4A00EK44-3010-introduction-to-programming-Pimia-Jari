273cb7ec5a61e2d6f20a6391286d22b256f4d1f3
JPimia <jari.pimia@tuni.fi>
2021-11-16 20:39:20 +0200

    10th Task

---
+++ homework/2021-46/e10/Main.java
@@ -0,0 +1,25 @@
+
+import javax.swing.*;
+import java.awt.event.*;
+
+class MyWindow extends JFrame {
+    private JButton click;
+    public MyWindow() {
+        click = new JButton("click");
+        click.addActionListener(this::clicked);
+        // click.addActionListener(e -> clicked(e));
+        add(click);
+    }
+    public void clicked(ActionEvent e) {
+        System.out.println("clicked");
+    }
+}
+
+class Main {
+    public static void main(String [] args) {
+        MyWindow window = new MyWindow();
+        window.setTitle("window");
+        window.setSize(400,400);
+        window.setVisible(true);
+    }
+}
